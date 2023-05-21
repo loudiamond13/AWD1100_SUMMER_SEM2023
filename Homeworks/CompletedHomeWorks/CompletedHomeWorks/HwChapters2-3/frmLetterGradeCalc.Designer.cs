bd934bb24595dcf8410761143ccbea397161b637
loudiamond13 <loudiamond_loyloy@insideranken.org>
2023-05-21 11:33:09 -0500

    HomeWorks_Ch02-03

---
+++ Homeworks/CompletedHomeWorks/CompletedHomeWorks/HwChapters2-3/frmLetterGradeCalc.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace HwChapters2_3
+{
+    partial class frmLetterGradeCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblNumberGrade = new System.Windows.Forms.Label();
+            this.lblLetterGrade = new System.Windows.Forms.Label();
+            this.txtNumberGrade = new System.Windows.Forms.TextBox();
+            this.txtLetterGrade = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Tempus Sans ITC", 20.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(62, 345);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(278, 76);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Font = new System.Drawing.Font("Tempus Sans ITC", 20.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(372, 345);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(278, 76);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Tempus Sans ITC", 20.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(693, 345);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnExit.Size = new System.Drawing.Size(278, 76);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblNumberGrade
+            // 
+            this.lblNumberGrade.AccessibleName = "";
+            this.lblNumberGrade.BackColor = System.Drawing.SystemColors.Info;
+            this.lblNumberGrade.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNumberGrade.Font = new System.Drawing.Font("Tempus Sans ITC", 15.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumberGrade.Location = new System.Drawing.Point(62, 99);
+            this.lblNumberGrade.Name = "lblNumberGrade";
+            this.lblNumberGrade.Size = new System.Drawing.Size(278, 72);
+            this.lblNumberGrade.TabIndex = 5;
+            this.lblNumberGrade.Text = "Number Grade :";
+            this.lblNumberGrade.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblLetterGrade
+            // 
+            this.lblLetterGrade.AccessibleName = "";
+            this.lblLetterGrade.BackColor = System.Drawing.SystemColors.Info;
+            this.lblLetterGrade.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLetterGrade.Font = new System.Drawing.Font("Tempus Sans ITC", 15.85714F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLetterGrade.Location = new System.Drawing.Point(62, 218);
+            this.lblLetterGrade.Name = "lblLetterGrade";
+            this.lblLetterGrade.Size = new System.Drawing.Size(278, 72);
+            this.lblLetterGrade.TabIndex = 6;
+            this.lblLetterGrade.Text = "Letter Grade :";
+            this.lblLetterGrade.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblLetterGrade.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtNumberGrade
+            // 
+            this.txtNumberGrade.Font = new System.Drawing.Font("Tempus Sans ITC", 20.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumberGrade.Location = new System.Drawing.Point(373, 98);
+            this.txtNumberGrade.Name = "txtNumberGrade";
+            this.txtNumberGrade.Size = new System.Drawing.Size(363, 69);
+            this.txtNumberGrade.TabIndex = 0;
+            this.txtNumberGrade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNumberGrade.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtLetterGrade
+            // 
+            this.txtLetterGrade.Font = new System.Drawing.Font("Tempus Sans ITC", 20.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLetterGrade.Location = new System.Drawing.Point(373, 219);
+            this.txtLetterGrade.Name = "txtLetterGrade";
+            this.txtLetterGrade.ReadOnly = true;
+            this.txtLetterGrade.Size = new System.Drawing.Size(363, 69);
+            this.txtLetterGrade.TabIndex = 4;
+            this.txtLetterGrade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmLetterGradeCalc
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(996, 519);
+            this.Controls.Add(this.txtLetterGrade);
+            this.Controls.Add(this.txtNumberGrade);
+            this.Controls.Add(this.lblLetterGrade);
+            this.Controls.Add(this.lblNumberGrade);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "frmLetterGradeCalc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Letter Grade Calculator";
+            this.Load += new System.EventHandler(this.frmLetterGradeCalc_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblNumberGrade;
+        private System.Windows.Forms.Label lblLetterGrade;
+        private System.Windows.Forms.TextBox txtNumberGrade;
+        private System.Windows.Forms.TextBox txtLetterGrade;
+    }
+}
+
---
+++ Homeworks/CompletedHomeWorks/CompletedHomeWorks/HwChapters2-3/frmLetterGradeCalc.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows.Forms;
+
+/*  Extra 2-1 Design a simple form
+
+    In this exercise, you’ll design a form that lets the user enter 
+    a number grade and then displays the letter grade when the user clicks the
+    Calculate button.
+*/
+
+namespace HwChapters2_3
+{
+    public partial class frmLetterGradeCalc : Form
+    {
+        public frmLetterGradeCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show(
+                "Do You Really Want To Exit The Program?",
+                "EXIT NOW?",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (dialog == DialogResult.Yes) 
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNumberGrade.Text = "";
+            txtLetterGrade.Text = "";
+            txtNumberGrade.Focus();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal numberGrade = Convert.ToDecimal(txtNumberGrade.Text);
+
+            string letterGrade = "";
+
+            if (numberGrade >= 88)
+            {
+
+                letterGrade = "A";
+            }
+
+            else if (numberGrade >= 80 && numberGrade <= 87)
+            {
+                letterGrade = "B";
+            }
+
+            else if (numberGrade >= 68 && numberGrade <= 79)
+            {
+                letterGrade = "C";
+            }
+
+            else if (numberGrade >= 60 && numberGrade <= 67)
+            {
+                letterGrade = "D";
+            }
+
+            else
+            {
+                letterGrade = "F";
+            }
+
+            txtLetterGrade.Text = letterGrade;
+            txtNumberGrade.Focus();
+        }
+
+        private void frmLetterGradeCalc_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
