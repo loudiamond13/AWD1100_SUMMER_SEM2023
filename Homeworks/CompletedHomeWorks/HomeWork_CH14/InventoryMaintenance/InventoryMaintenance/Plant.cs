73471423625f834c3d5ad7964ed6edbc44c49a0a
loudiamond13 <loudiamond_loyloy@insideranken.org>
2023-08-26 10:24:00 -0500

    HomeWorks

---
+++ Homeworks/CompletedHomeWorks/HomeWork_CH14/InventoryMaintenance/InventoryMaintenance/Plant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class Plant : InventoryItem
+    {
+        //getters & Setters
+        public string Size { get; set; } = "";
+
+
+        //full argument constructor
+        public Plant(int itemNo, string description, decimal price, string size) 
+                    : base(itemNo, description, price)  
+        {
+            Size = size;
+
+        }
+
+        public override string GetDisplayText()
+        {
+            return ItemNo.ToString() + "    " + Size + "    " + Description + "    " + Price.ToString("c");
+        }
+    }
+}
---
+++ Homeworks/CompletedHomeWorks/HomeWork_CH14/InventoryMaintenance/InventoryMaintenance/Supply.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    internal class Supply : InventoryItem
+    {
+        
+
+        //full argument constructor
+        public Supply(int itemNo, string description, decimal price, string manufacturer)
+                        : base (itemNo, description, price)
+        {
+            Manufacturer = manufacturer;
+        }
+
+        //get set
+        public string Manufacturer { get; set; } = "";
+
+        public override string GetDisplayText()
+        {
+            return ItemNo.ToString() + "    " + Manufacturer + "    " + Description + "    " + Price.ToString("c");
+        }
+
+    }
+}
